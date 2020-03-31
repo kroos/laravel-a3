3dd76a0be54499f16516fb15e76e9fed66afb4f3
Kroos <dhiauddin@gmail.com>
2020-03-31 20:37:03 +0800

    - fixing on blade - nothing develop yet for a3

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -60,12 +60,12 @@
 			</div>
 
 			<div class="form-group row mb-0">
-				<div class="col-md-8 offset-md-4">
-					<button type="submit" class="btn btn-primary btn-block">
+				<div class="col-md-6 offset-md-4">
+					<button type="submit" class="btn btn-primary">
 						{{ __('Login') }}
 					</button>
 
-					<a class="btn btn-link" href="{{ route('password.request') }}">
+					<a class="btn btn-primary" href="{{ route('password.request') }}">
 						{{ __('Forgot Your Password?') }}
 					</a>
 				</div>
