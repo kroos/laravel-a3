a7551955081cd030ac59727d518324155ece77b4
dhiauddin <dhiauddin@gmail.com>
2023-07-01 13:09:08 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -189,16 +189,3 @@ Route::post('serial_list', 'GMController@serialliststore')->name('seriallist.sto
 
 // item calculator
 Route::get('item_calculator', 'GMController@itemcalculator')->name('itemcalculator.index');
-
-
-
-
-
-
-
-
-
-
-// https://e-hentai.org/?f_search=artist%3A%22azuki+kurenai%24%22%3Blanguage%3A%22english%24%22
-// https://e-hentai.org/?f_cats=505&f_search=language%3A%22english%24%22%3Bfemale%3A%22big+breast%24%22%3Bfemale%3A%22milf%24%22%3Bmale%3A%22big+penis%24%22%3Bfemale%3A%22anal%24%22
-// https://hdsex.org/video/139501197?utm_source=xdiver&utm_medium=click&utm_campaign=xdiver
